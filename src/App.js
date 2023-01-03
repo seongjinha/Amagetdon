64ed172ff5b61dcc1e07da0249b4c9acc7dad7d7
JinHwanCha <wlsksl103@gmail.com>
2023-01-03 20:24:42 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Login from "./Login";
+import Logo from "./Logo";
+function App() {
+  return (
+    <div className='page'>
+      <Header />
+      <Login />
+      <Logo />
+    </div>
+  );
+}
+export default App;
---
+++ src/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Logo() {
+    return (
+        <div className='logo-box'>
+            <h1 className='logo'>Project heeha</h1>
+        </div>
+    )
+}
